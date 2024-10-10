44f2329b291594176057e280193885e2bf101b99
Aminat Baderinwa <yetami_jewel@yahoo.com>
2024-10-10 23:46:47 +0100

    edited to indicate data types

--- oop.py
+++ oop.py
@@ -1,14 +1,14 @@
 class Product:
-    def __init__(self,name,price,stock,weight):
+    def __init__(self,name: str,price: float,stock: int,weight: float):
         self.name = name
         self.price = price
         self.stock = stock
         self.weight = weight
 
-    def get_info(self):
+    def get_info(self) -> str :
         return f"Product Name: {self.name}, Price: ${self.price}, Stock: {self.stock}, Weight: {self.weight} kg"
 
-    def apply_discount(self,discount_percentage):
+    def apply_discount(self,discount_percentage) -> None:
         self.price -= discount_percentage * self.price
 
     def check_stock(self,quantity):
@@ -19,25 +19,25 @@ class Product:
             return {"Out of stock"}
         
     def calculate_shipping_cost(self):
-        shipping_cost = 10 * self.weight
+        shipping_cost: float = 10 * self.weight
         return f"shipping cost for {self.name}:${shipping_cost}"
 
 
 class ElectronicProduct(Product):
-    def __init__(self,name,price,stock,weight,warranty_years,brand):
+    def __init__(self,name,price,stock,weight,warranty_years: int,brand: str):
         super().__init__(name,price,stock,weight)
         self.warranty_years  = warranty_years
         self.brand = brand
     
     def get_info(self):
-        return f"Electronic Product: {self.name}: {self.brand}, Price: ${self.price}, Warranty: {self.warranty_years} Stock: {self.stock}, Weight: {self.weight} kg"
+        return f"Electronic Product: {self.name}: {self.brand}, Price: ${self.price}, Warranty: {self.warranty_years}years Stock: {self.stock}, Weight: {self.weight} kg"
     
     def calculate_warranty_extension(self,extra_years):
         self.warranty_years += extra_years
         return f"Extended warranty for {self.name} is {self.warranty_years} years"
 
 class GrocreyProduct(Product):
-    def __init__(self,name,price,stock,weight,expiration_date,is_perishable):
+    def __init__(self,name,price,stock,weight,expiration_date: str,is_perishable: bool):
         super().__init__(name,price,stock,weight)
         self.expiration_date  = expiration_date
         self.is_perishable = is_perishable
@@ -53,8 +53,8 @@ class GrocreyProduct(Product):
         
 
 
-Laptop = ElectronicProduct("Laptop",1500,10,2.5,3,"Dell")
-Milk = GrocreyProduct("Milk",4,50,1,"2024-11-15","True")
+Laptop = ElectronicProduct("Laptop",1500.00,10,2.5,3,"Dell")
+Milk = GrocreyProduct("Milk",4.00,50,1.0,"2024-11-15","True")
 
 print(Laptop.get_info())
 Laptop.apply_discount(0.15)
